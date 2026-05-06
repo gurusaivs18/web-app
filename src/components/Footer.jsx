f366789d7c19f87eef25df55f53744e247ba885e
gurusaivs18 <gurusaivs@gmail.com>
2026-05-06 10:01:45 +0400

    css-grids breaking issue

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,164 +1,49 @@
-// import { Link } from "react-router-dom";
-// import { FaFacebookF, FaInstagram, FaLinkedinIn } from "react-icons/fa";
-// import "../css/Footer.css";
-// import Logo from "./Logo";
-
-// function Footer() {
-//   return (
-//     <footer className="footer">
-//       <div className="container">
-//         <div className="footer-top">
-//           {/* Address */}
-//           <div className="footer-col footer-address">
-//             <div className="footer-logo">
-//               <Logo />
-//             </div>
-//             <p>Office Location:</p>
-//             <p>Oaks Liwa Heights Office Tower </p>
-//             <p> 24th Floor, 2404</p>
-//             <p>Cluster W, JLT</p>
-//             <p>Dubai, UAE</p>
-//             <p>050-6785243</p>
-//           </div>
-
-//           {/* Company Links */}
-//           <div className="footer-col">
-//             <h4>Company</h4>
-//             <ul>
-//               <li>
-//                 <Link to="/about">About us</Link>
-//               </li>
-//               <li>
-//                 <Link to="/about">Team</Link>
-//               </li>
-//               <li>
-//                 <Link to="/verticals">Service</Link>
-//               </li>
-//               <li>
-//                 <Link to="/careers">Career</Link>
-//               </li>
-//               <li>
-//                 <Link to="/contact">Contact us</Link>
-//               </li>
-//             </ul>
-//           </div>
-
-//           {/* Quick Links (External → New Tab) */}
-//           <div className="footer-col">
-//             <h4>Quick Links</h4>
-//             <ul>
-//               <li>
-//                 <a
-//                   href="https://www.snapfitness.com/ae"
-//                   target="_blank"
-//                   rel="noopener noreferrer"
-//                 >
-//                   Snap Fitness
-//                 </a>
-//               </li>
-//               <li>
-//                 <a
-//                   href="https://nbventuresme.com/"
-//                   target="_blank"
-//                   rel="noopener noreferrer"
-//                 >
-//                   NB Venture
-//                 </a>
-//               </li>
-//               <li>
-//                 <a
-//                   href="https://vktechnologiesme.com/"
-//                   target="_blank"
-//                   rel="noopener noreferrer"
-//                 >
-//                   Vanity Kart
-//                 </a>
-//               </li>
-//               <li>
-//                 <a
-//                   href="https://pressmans.com/"
-//                   target="_blank"
-//                   rel="noopener noreferrer"
-//                 >
-//                   Pressmans
-//                 </a>
-//               </li>
-//             </ul>
-//           </div>
-
-//           {/* Social Links */}
-//           <div className="footer-col">
-//             <h4>Follow Us</h4>
-//             <div className="footer-social">
-//               {/* <a href="#" target="_blank" rel="noopener noreferrer">
-//                 <FaFacebookF />
-//               </a>
-//               <a href="#" target="_blank" rel="noopener noreferrer">
-//                 <FaInstagram />
-//               </a> */}
-//               <a href="#" target="_blank" rel="noopener noreferrer">
-//                 <FaLinkedinIn />
-//               </a>
-//             </div>
-//           </div>
-//         </div>
-
-//         {/* Footer Bottom */}
-//         <div className="footer-bottom">
-//           <p>© {new Date().getFullYear()} JSB Group. All rights reserved.</p>
-//         </div>
-//       </div>
-//     </footer>
-//   );
-// }
-
-// export default Footer;
-
 import { Link } from "react-router-dom";
-import { FaFacebookF, FaInstagram, FaLinkedinIn } from "react-icons/fa";
+import { FaLinkedinIn } from "react-icons/fa";
 import "../css/Footer.css";
 import Logo from "./Logo";
 
 function Footer() {
   return (
     <footer className="footer">
-      <div className="container">
-        <div className="footer-top">
-          {/* Address */}
-          <div className="footer-col footer-address">
+      <div className="footer-container">
+        {/* TOP */}
+        <div className="footer-grid">
+          {/* ADDRESS */}
+          <div className="footer-col footer-about">
             <Logo />
 
-            <p>Office Location:</p>
+            <p className="muted">Office Location:</p>
             <p>Oaks Liwa Heights Office Tower</p>
             <p>24th Floor, 2404</p>
             <p>Cluster W, JLT</p>
             <p>Dubai, UAE</p>
-            <p>050-6785243</p>
+            <p className="highlight">050-6785243</p>
           </div>
 
-          {/* Company */}
+          {/* COMPANY */}
           <div className="footer-col">
             <h4>Company</h4>
             <ul>
               <li>
-                <Link to="/about">About us</Link>
+                <Link to="/about">About Us</Link>
               </li>
               <li>
                 <Link to="/about">Team</Link>
               </li>
               <li>
-                <Link to="/verticals">Service</Link>
+                <Link to="/verticals">Services</Link>
               </li>
               <li>
-                <Link to="/careers">Career</Link>
+                <Link to="/careers">Careers</Link>
               </li>
               <li>
-                <Link to="/contact">Contact us</Link>
+                <Link to="/contact">Contact</Link>
               </li>
             </ul>
           </div>
 
-          {/* Quick Links */}
+          {/* QUICK LINKS */}
           <div className="footer-col">
             <h4>Quick Links</h4>
             <ul>
@@ -201,11 +86,11 @@ function Footer() {
             </ul>
           </div>
 
-          {/* Social */}
+          {/* SOCIAL */}
           <div className="footer-col">
             <h4>Follow Us</h4>
 
-            <div className="footer-social">
+            <div className="socials">
               <a href="#" target="_blank" rel="noreferrer">
                 <FaLinkedinIn />
               </a>
@@ -213,7 +98,7 @@ function Footer() {
           </div>
         </div>
 
-        {/* Bottom */}
+        {/* BOTTOM */}
         <div className="footer-bottom">
           <p>© {new Date().getFullYear()} JSB Group. All rights reserved.</p>
         </div>
