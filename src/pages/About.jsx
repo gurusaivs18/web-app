a648c64a36b74859d5e050e611ea0aad97281690
gurusaivs18 <gurusaivs18@gmail.com>
2026-09-22 18:09:11 +0400

    Fix asset imports

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -13,7 +13,7 @@ import { createPortal } from "react-dom";
 import sanalImg from "../assets/Partners/sanal-assets-1.webp";
 import supriyaImg from "../assets/Partners/supriya-assets-1.png";
 import naz from "../assets/Partners/Nas-asset-1.png";
- import ashik from "../assets/Partners/Ashik-asset-1.png";
+import ashik from "../assets/Partners/Ashik-asset-1.png";
 import visionImg from "../assets/About us/vision.png";
 import missionImg from "../assets/About us/mission.png";
 import purposeImg from "../assets/About us/purpose.png";
